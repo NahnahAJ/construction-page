603dd6892abd81ea5da459ae23a96a41e1235c95
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-15 11:51:44 -0700

    Add javascript code to open and close menu

--- menu.js
+++ menu.js
@@ -2,32 +2,32 @@ staff = [
   { image: '/Icons/constructor-2.jpg',
   name: 'Hilary Doe',
   tags: 'CEO & Co-founder',
-  about: 'A lot of text'
+  about: 'After starting out the company as a regular construction comapny, Hilary saw to make a change in how buildings are made so as to sustain the environment. With his vision, Eco-Builders was formed.'
   },
   { image:'./Icons/c.jpg',
   name: 'Mark Olsen',
   tags: 'Chief Architect',
-  about: 'A lot of text'
+  about: 'Mark brings technical and creative skills together to create plans for residential and commercial buildings. This aids in meeting the wishes of clients and ensuring compliance with building regulations.'
   },
   { image:'./Icons/constructor-3.jpg',
   name: 'John James',
   tags: 'Construction Manager',
-  about: 'A lot of text'
+  about: 'James and his team perform a critical role in the running of any construction or renovation project. His teams skills and knowledge largely determine if a project is completed on time and within budget.'
   },
   { image:'./Icons/constructor-4.jpg',
   name: 'Janet Jumah',
-  tags: 'Head of Marketing',
-  about: 'A lot of text'
+  tags: 'Site Manager ',
+  about: 'Janet oversees work schedules and coordinating the arrival of materials and labour to the construction site. She operates as a liaison between boots on the ground and the companys management.'
   },
   { image:'./Icons/constructor-5.jpg',
   name: 'Lena Perry',
   tags: 'Interior Designer',
-  about: 'A lot of text'
+  about: 'Lena is in charge of sketch designs and ideas using traditional methods or computer software. She and her team work to a brief, following design principles to produce exemplary results.'
   },
-  { image:'./Icons/constructor-5.jpg',
-  name: 'Lena Perry',
-  tags: 'Interior Designer',
-  about: 'A lot of text'
+  { image:'./Icons/constructor-6.jpg',
+  name: 'Francis Drew',
+  tags: 'Head Engineer',
+  about: 'Francis and his team use their training to design, construct, and test structures and problem-solving systems. They specialise in particular areas and drive innovation to meet both client demands environmental demands.'
   }
 ]
  let staffCard = '';
@@ -42,8 +42,31 @@ const main = document.getElementById('featured-speakers')
 const workers = document.createElement('div')
 workers.className = 'workers';
 workers.innerHTML = staffCard;
-
 main.append(workers);
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu-nav');
+const close = document.querySelector('.close');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('display');
+  hamburger.classList.toggle('hidden');
+  document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', () => {
+  navMenu.classList.toggle('display');
+  hamburger.classList.toggle('hidden');
+  document.body.style.overflow = 'auto';
+});
+
+document.querySelectorAll('.menu-link').forEach((n) => n.addEventListener('click', () => {
+  navMenu.classList.toggle('display');
+  hamburger.classList.toggle('hidden');
+  document.body.style.overflow = 'auto';
+}));
 
 
 
